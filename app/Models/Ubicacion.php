7fd305224f686883afc2538a87c39a7a7499901c
Genesis <genesis.dmop@gmail.com>
2022-09-02 12:46:43 -0500

    modelos corregidos

--- app/Models/Ubicacion.php
+++ app/Models/Ubicacion.php
@@ -4,24 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ubicacion extends Model
 {
-    use HasFactory;
+    use SoftDeletes, HasFactory;
+
+    protected $table = 'ubicaciones';
+    public $timestamps = true;
+
+    protected $dates = ['deleted_at'];
+    protected $fillable = array('tipo','nombreubicaciones','cofeprisfunc','cofeprispubli','email','telefonocitas','tipotelefono','telefonoemergencias','imagenubicacion','tipovialidad','nombrevialidad','numeroexterior','numerointerior',' tipocolonia','nombrecolonia','cp','cliente_id','info_fiscal_id','ciudad_id','estado_id','pais_id','creadopor','actualizadopor');
 
     public function CaracteristicasUbicacion(){
-        return $this->hasOne(CaracteristicasUbicacion::class);
+        return $this->hasOne('App\Models\CaracteristicasUbicacion');
     }
 
     public function Ciudad(){
-        return $this->hasOne(Ciudad::class);
+        return $this->hasOne('App\Models\Ciudad');
     }
 
     public function Cliente(){
-        return $this->belongsTo(Cliente::class);
+        return $this->belongsTo('App\Models\Cliente');
     }
 
     public function InfoFiscal(){
-        return $this->belongsTo(InfoFiscal::class);
+        return $this->belongsTo('App\Models\InfoFiscal');
     }
 }
