87e0e193b7041f0c1164cde555087b4e3e31f619
Josdavs29 <josdavsm88@gmail.com>
2022-09-21 14:40:26 -0500

    correcciones

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
     public $timestamps = false;
 
     protected $dates = ['deleted_at'];
-    protected $fillable = array('username','email','password','nombre','apellidopaterno','apellidomaterno','telefonopersonal','fechanacimiento','edad','genero','ciudad_id','estado_id','pais_id','creadopor','actualizadopor');
+    protected $fillable = array('username','email','password','nombre','apellidopaterno','apellidomaterno','telefonopersonal','fechanacimiento','edad','genero','municipio_id','estado_id','pais_id','creadopor','actualizadopor');
 
     public function roles(){
         return $this->belongsToMany('App\\Models\Role','role_user');
