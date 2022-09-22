970790807333f79135591b9638bf1648e6b1b61d
Josdavs29 <josdavsm88@gmail.com>
2022-09-22 12:28:06 -0500

    show user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -112,12 +112,75 @@ class UserController extends Controller
     }
     */
 
-    public function userProfile(){
-        return response()->json([
-            "status" => 1,
-            "msg" => "Acerca del usuario",
-            "data"=> auth()->user()
-        ]);
+    public function index(Request $request){
+        try {
+            $user_id = auth()->user()->id;
+            /* $user = User::find(1); */
+            
+            $todolodemas = [];
+            $limit = env('PAGINATION_LIMIT', 20);
+            $maxPaginationLimit = env('MAX_PAGINATION_LIMIT', 500);
+            $order = 'id';
+            $direction = 'desc';
+        
+            if(isset($request->l)) $limit = $request->l > $maxPaginationLimit || $request->l == 0 ? $limit : $request->l;
+            if(isset($request->o)) $order = $request->o;
+            if(isset($request->d)) $direction = $request->d;
+            
+            if($user_id->puede($user_id,'cliente','r'))
+            {
+                $recurso = User::with('cliente')->paginate($limit);
+
+                if($recurso==null){
+                    $todolodemas['info']['mensaje'] = 'No se encontraron registros en la base de datos';
+                    $todolodemas['info']['infos'] = ['registros'=>['No se encontraron registros en la base de datos']];
+                    return (new Formatear)->igor($recurso,202,$todolodemas);
+                }
+                return (new Formatear)->igor($recurso,200,$todolodemas);
+            }
+            else{
+                $todolodemas['error']['mensaje'] = 'No cuenta con los permisos para este recurso';
+                $todolodemas['error']['errores'] = ['permisos'=>['No cuenta con los permisos para este recurso']];
+                return (new Formatear)->igor(null,403,$todolodemas);
+            }
+        } catch (\Throwable $th) {
+          $todolodemas['error']['mensaje'] = 'Error en el servidor, ocurrió un error inesperado';
+          $todolodemas['error']['errores'] = ['errorinesperado'=>[$th]];
+          return (new Formatear)->igor(null,500,$todolodemas);
+        }
+    }
+
+    public function show($id, Request $request){
+        try {
+            $user_id = auth()->user()->id;
+            //$user = User::find(1);
+        
+            $todolodemas = [];
+  
+            if($user_id->puede($user_id,'cliente','r'))
+            {
+  
+            $recurso = User::with('cliente')->find($id);
+            
+                if (is_null($recurso)) {
+                    $todolodemas['info']['mensaje'] = 'No se encontró el registro buscado en la base de datos';
+                    $todolodemas['info']['infos'] = ['registros'=>['No se encontró el registro buscado en la base de datos']];
+                    return (new Formatear)->igor($recurso,202,$todolodemas);
+                }
+  
+                return (new Formatear)->igor($recurso,200,$todolodemas);
+            }
+            else
+            {
+                $todolodemas['error']['mensaje'] = 'No cuenta con los permisos para este recurso';
+                $todolodemas['error']['errores'] = ['permisos'=>['No cuenta con los permisos para este recurso']];
+                return (new Formatear)->igor(null,403,$todolodemas);
+            }
+        } catch (\Throwable $th) {
+            $todolodemas['error']['mensaje'] = 'Error en el servidor, ocurrió un error inesperado';
+            $todolodemas['error']['errores'] = ['errorinesperado'=>[$th]];
+            return (new Formatear)->igor(null,500,$todolodemas);
+        }
     }
 
     public function logout(){
