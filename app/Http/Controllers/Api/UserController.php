7441aa1774b7c5b9cbcda65d5bce458120197d12
Josdavs29 <josdavsm88@gmail.com>
2022-09-22 10:44:42 -0500

    cambios

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,56 +5,23 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB; 
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function register(Request $request){
-        $request->validate([
-            'username' => 'required|unique:users',
-            'email' => 'required|email|unique:users',
-            'password' => 'required',
-            'nombre' => 'required',
-            'apellidopaterno' => 'required',
-            'apellidomaterno' => 'required',
-            'telefonopersonal' => 'required',
-            'fechanacimiento' => 'required',
-            'edad' => 'required',
-            'genero' => 'required',
-        ]);
+        try {
+            $user = User::find(1);
+            
+            $todolodemas = [];
 
-        $user =new User();
-        $user->username = $request->username;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->nombre = $request->nombre;
-        $user->apellidopaterno = $request->apellidopaterno;
-        $user->apellidomaterno = $request->apellidomaterno;
-        $user->telefonopersonal = $request->telefonopersonal;
-        $user->fechanacimiento = $request->fechanacimiento;
-        $user->edad = $request->edad;
-        $user->genero = $request->genero;
-
-        /*
-        {
-            "username" : "josdav",
-            "email" : "josdav@gmail.com",
-            "password" : "123456",
-            "nombre" : "josue",
-            "apellidopaterno" : "sarmiento",
-            "apellidomaterno" : "montero",
-            "telefonopersonal" : "96112345678",
-            "fechanacimiento" : "2000-10-04",
-            "edad" : "21",
-            "genero" : "masculino",              
-        }
-        */
-        $user->save();
-
-        return response()->json([
-            "status" =>1,
-            "msg" => "Registro exitoso",
-        ]);
+            if($user->puede($user,'')){
+                DB::beginTransaction();
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+        }    
     }
 
     public function login(Request $request){
